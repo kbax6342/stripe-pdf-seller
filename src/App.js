90359adfea8a7372eb70cf3ae6ad7d10d6c9ddf1
Kevin Nelms Baxter II <k3vin.baxt3r@gmail.com>
2025-12-05 21:12:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -38,9 +38,13 @@ function App() {
       <header className="App-header">
         <h1 style={{ margin: 0, padding: 0 }}>Multimango Begginners Guide PDF</h1>
         <h2 style={{ margin: 0, padding: 0 }}>Exclusive PDF Download</h2>
-        <p style={{  padding: 0 }}>Get instant access to our premium digital guide!</p>
-        <stripe-buy-button
-  buy-button-id="buy_btn_1SbA6t1jrXLjDwD570JTJ91j"
+        <p style={{ padding: 0 }}>Get instant access to our premium digital guide!</p>
+        <script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1SbAqS1jrXLjDwD5Bhr9ExvT"
   publishable-key="pk_live_51S1pGm1jrXLjDwD5SpGd6HK6z7RRbM3kO2C9V5YsnaSDs0prhJPXcncBqtSWLam7rWksxUUiexnftH2KZChHu7Q600yMMP3QbO"
 >
 </stripe-buy-button>
